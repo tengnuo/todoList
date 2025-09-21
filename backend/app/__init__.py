62d53a012cf7d2450a094b62d63ea830430835b6
tengnuo <2648225921@qq.com>
2025-09-21 16:35:52 +0800

    整理项目骨架，完成目录结构

---
+++ backend/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+
+    db.init_app(app)
+
+    # 蓝图注册
+    from .routes.tasks import tasks_bp
+    app.register_blueprint(tasks_bp, url_prefix="/tasks")
+
+    return app
